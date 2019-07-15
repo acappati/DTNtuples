ca0198a1d2258043fbe2465e859735eb1f012398
alessandra <alessandra.cappati@cern.ch>
2019-07-15 16:53:19 +0200

    update plot script

--- test/Plotter.C
+++ test/Plotter.C
@@ -38,21 +38,53 @@
 #include "TPaveText.h"
 
 
-void doThePlots(){
+void doThePlots(TString inputFile, TString outputPathPlots)
+{
+  // read in file
+  TFile *inFile = TFile::Open("results.root");
+
+  // take histos
+  TH1F* h_Ph2DigiWithoutPh1        = (TH1F*)inFile->Get("h_Ph2DigiWithoutPh1");
+  TH1F* h_Ph2DigiMinusPh1Digi      = (TH1F*)inFile->Get("h_Ph2DigiMinusPh1Digi");
+  TH1F* h_Ph2DigiMinusPh1Digi_zoom = (TH1F*)inFile->Get("h_Ph2DigiMinusPh1Digi_zoom");
+
+
+  // plot histos
+  TCanvas* c_Ph2DigiWithoutPh1 = new TCanvas("c_Ph2DigiWithoutPh1","c_Ph2DigiWithoutPh1");
+  c_Ph2DigiWithoutPh1->cd();
+  h_Ph2DigiWithoutPh1->Draw();
+  c_Ph2DigiWithoutPh1->SaveAs(outputPathPlots + "/" + c_Ph2DigiWithoutPh1->GetName() + ".png");
+
+  TCanvas* c_Ph2DigiMinusPh1Digi = new TCanvas("c_Ph2DigiMinusPh1Digi","c_Ph2DigiMinusPh1Digi");
+  c_Ph2DigiMinusPh1Digi->cd();
+  h_Ph2DigiMinusPh1Digi->Draw();
+  c_Ph2DigiMinusPh1Digi->SaveAs(outputPathPlots + "/" + c_Ph2DigiMinusPh1Digi->GetName() + ".png");
+
+  TCanvas* c_Ph2DigiMinusPh1Digi_zoom = new TCanvas("c_Ph2DigiMinusPh1Digi_zoom","c_Ph2DigiMinusPh1Digi_zoom");
+  c_Ph2DigiMinusPh1Digi_zoom->cd();
+  h_Ph2DigiMinusPh1Digi_zoom->Draw();
+  c_Ph2DigiMinusPh1Digi_zoom->SaveAs(outputPathPlots + "/" + c_Ph2DigiMinusPh1Digi_zoom->GetName() + ".png");
+  
+  
+
+   
+  
  
- 
+  cout<<" Plots done =) "<<endl;
 
 }
 
 
-void Plotter(){
+void Plotter()
+{
 
   TString inputFile = "results.root";
 
   // define output directory
-  TString outputPathPlots = "190712_plots";
+  TString outputPathPlots = "~/www/DTDPG/190715_plots";
   gSystem->Exec("mkdir -p "+outputPathPlots);
 
-  
+  // call funciton for plots
+  doThePlots(inputFile, outputPathPlots);  
 
 }
